2025853d156647346550d49b4234e704202710be
aXuan0915 <1924485178@qq.com>
2026-05-21 22:21:30 +0800

    更新 SITE_URL 为 Vercel 实际域名

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "我的博客";
+export const SITE_DESCRIPTION = "一个关于 Web 开发与技术的个人博客。";
+export const SITE_URL = "https://my-blog-five-blond.vercel.app";
+export const POSTS_PER_PAGE = 10;
+export const CONTENT_DIR = "content/posts";
